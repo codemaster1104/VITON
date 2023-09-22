f4102e73ad679ccf6c65dc9483217467560ae88e
codemaster1104 <115527374+codemaster1104@users.noreply.github.com>
2023-09-22 23:38:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import cvzone
+import cv2
+
+import os
+
+import cvzone
+import cv2
+from cvzone.PoseModule import PoseDetector
+
+cap = cv2.VideoCapture(0)
+detector = PoseDetector()
+
+shirtFolderPath = "Resources/Shirts"
+listShirts = os.listdir(shirtFolderPath)
+# print(listShirts)
+fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
+shirtRatioHeightWidth = 581 / 440
+imageNumber = 0
+imgButtonRight = cv2.imread("Resources/button.png", cv2.IMREAD_UNCHANGED)
+imgButtonLeft = cv2.flip(imgButtonRight, 1)
+counterRight = 0
+counterLeft = 0
+selectionSpeed = 10
+
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    # img = cv2.flip(img,1)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
+    if lmList:
+        # center = bboxInfo["center"]
+        lm11 = lmList[11][1:3]
+        lm12 = lmList[12][1:3]
+        imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
+
+        widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
+        print(widthOfShirt)
+        try:
+            imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
+        except cv2.error as e:
+            print("Error resizing image:", e)
+        currentScale = (lm11[0] - lm12[0]) / 190
+        offset = int(44 * currentScale), int(48 * currentScale)
+
+        try:
+            img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
+        except:
+            pass
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
